da7eeeee0b4ea64e5c290335c562bb468ee0d018
Gakkykun <i.yoshihiko@protonmail.com>
2022-07-26 00:46:21 +0000

    Modify app_server.R

--- R/app_server.R
+++ R/app_server.R
@@ -9,11 +9,11 @@ app_server <- function(input, output, session) {
   output$distPlot <- renderPlot({
 
     stats <- cranlogs::cran_downloads("lazytrade", from=input$daterange4[1], to=input$daterange4[2])
-    ggplot2::ggplot(stats, aes(date, count)) +
+    ggplot2::ggplot(stats, ggplot2::aes(date, count)) +
       ggplot2::geom_col() +
       ggplot2::labs(
         title=sprintf(
-          "{stats} downloads to %s",
+          "{stats} downloads of lazytrade R package to %s",
           Sys.Date() - 1
         ),
         caption = "dat from {cranlogs}"
